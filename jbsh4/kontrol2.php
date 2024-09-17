31d8d6958b438e7745c128b70587d2c4aaa2e929
Muhammad Afif Khosyidzaki <afifkhosyidzaki@gmail.com>
2024-09-17 10:29:28 +0700

    commit js4 kontrol2 question 2

---
+++ jbsh4/kontrol2.php
@@ -0,0 +1,14 @@
+<?php
+$originalPrice = 120000;
+$discountPercentage = 20;
+
+
+$discountAmount = ($discountPercentage / 100) * $originalPrice;
+$finalPrice = $originalPrice - $discountAmount;
+
+
+echo "Original Price: Rp " . number_format($originalPrice, 0, ',', '.') . "<br>";
+echo "Discount: " . $discountPercentage . "%<br>";
+echo "Discount Amount: Rp " . number_format($discountAmount, 0, ',', '.') . "<br>";
+echo "Final Price to be Paid: Rp " . number_format($finalPrice, 0, ',', '.') . "<br>";
+?>
