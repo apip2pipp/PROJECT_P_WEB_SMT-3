022da02103daef11e75c797d669d18c094bcedda
Muhammad Afif Khosyidzaki <afifkhosyidzaki@gmail.com>
2024-09-17 10:36:27 +0700

    js 4 kontrol3 question 3

---
+++ jbsh4/kontrol3.php
@@ -0,0 +1,9 @@
+<?php
+$totalPoints = 650; 
+echo "Player's total score is: $totalPoints <br>";
+if ($totalPoints > 500) {
+    echo "Do players get additional rewards? (YES) <br>";
+} else {
+    echo "Do players get additional rewards? (NO) <br>";
+}
+?>
