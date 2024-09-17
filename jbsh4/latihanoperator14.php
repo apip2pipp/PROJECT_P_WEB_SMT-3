671a06692d9b055537399589e5716c127d15140a
Muhammad Afif Khosyidzaki <afifkhosyidzaki@gmail.com>
2024-09-17 10:08:53 +0700

    js 4 latihan dari operator

---
+++ jbsh4/latihanoperator14.php
@@ -0,0 +1,12 @@
+<?php
+
+$totalSeats = 45;
+$occupiedSeats = 28;
+$emptySeats = $totalSeats - $occupiedSeats;
+$percentageEmpty = ($emptySeats / $totalSeats) * 100;
+
+
+echo "Total Seats: $totalSeats<br>";
+echo "Occupied Seats: $occupiedSeats<br>";
+echo "Empty Seats: $emptySeats<br>";
+echo "Percentage of Empty Seats: " . round($percentageEmpty, 2) . "%<br>";
