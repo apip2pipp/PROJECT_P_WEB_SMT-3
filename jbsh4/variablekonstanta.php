f55e8431412c4670a6eda5e293975553295e100d
Muhammad Afif Khosyidzaki <afifkhosyidzaki@gmail.com>
2024-09-17 09:07:55 +0700

    js4 Constanta

---
+++ jbsh4/variablekonstanta.php
@@ -0,0 +1,20 @@
+<?php
+$angka1 = 10;
+$angka2 = 5;
+$hasil = $angka1 + $angka2;
+echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
+
+
+
+$benar = true;
+$salah = false;
+echo "Variabel benar: $benar, Variabel salah: $salah";
+
+
+
+
+define("NAMA_SITUS", "Webssitekupip.com");
+define("TAHUN_PENDIRIAN", 2024);
+
+echo "Selamat datang di " . NAMA_SITUS . ", situs yang didirikan pada tahun " . TAHUN_PENDIRIAN . ".";
+?>
