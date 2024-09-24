452a003fc2de1a4f7f3069e6c155ece5a978e301
Muhammad Afif Khosyidzaki <afifkhosyidzaki@gmail.com>
2024-09-24 08:27:29 +0700

    js5 first commit

---
+++ JS5/array1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ARRAY TERINDEKS</title>
+</head>
+<body>
+    <h2>Array Terindeks</h2>
+    <?php
+    $Listdosen = ["Elok nurhamadana", "Unggul pamenang", "Bagas Nugraha"];
+    foreach ($Listdosen as $dosen) {
+        echo $dosen . "<br>";
+    }
+    ?>
+</body>
+</html>
