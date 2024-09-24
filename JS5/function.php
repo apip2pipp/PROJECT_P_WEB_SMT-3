90f4b24b5e1fb6156c39ef434e5237e186a31498
Muhammad Afif Khosyidzaki <afifkhosyidzaki@gmail.com>
2024-09-24 09:29:41 +0700

    js 5 function modify jobsheet

---
+++ JS5/function.php
@@ -0,0 +1,12 @@
+<?php
+function perkenalan() {
+    echo "<div style='font-family: Arial, sans-serif; color: #333;'>";
+    echo "<h2>Assalamualaikum,</h2>";
+    echo "<p>Muhammad Afif Khosyidzaki pake i jangan z</p>"; // Tulis sesuai nama kalian
+    echo "<p>Senang berkenalan dengan kalian.</p>";
+    echo "</div><br/>";
+}
+
+// Memanggil fungsi yang sudah dibuat dua kali
+perkenalan();
+?>
