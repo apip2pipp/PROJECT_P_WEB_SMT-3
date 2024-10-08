faefeca43eb250a1b806d134fe5f61a79b0fdf71
Muhammad Afif Khosyidzaki <afifkhosyidzaki@gmail.com>
2024-10-08 10:51:38 +0700

    JS 7 -PRAKTIKUM2

---
+++ JS7/Function-empty/empty.php
@@ -0,0 +1,19 @@
+<?php
+$myArray = array(); // Array kosong
+
+if (empty($myArray)) {
+    echo "Array tidak terdefinisi atau kosong.";
+} else {
+    echo "Array terdefinisi dan tidak kosong.";
+}
+
+
+?>
+<br><br><br>
+<?php
+if (empty($nonExistentVar)) {
+    echo "Variabel tidak terdefinisi atau kosong.";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong.";
+}
+?>
