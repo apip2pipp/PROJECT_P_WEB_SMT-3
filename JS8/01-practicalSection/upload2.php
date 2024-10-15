cf8dc12cccf08bb9cc78c08a1c99c5e843d45ab9
Muhammad Afif Khosyidzaki <afifkhosyidzaki@gmail.com>
2024-10-15 11:27:52 +0700

    JS 8 prak 2

---
+++ JS8/01-practicalSection/upload2.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_POST["submit"])){
+    $targetdir = "uploads/"; // Direktori tujuan untuk menyimpan file
+    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $maxsize = 5*1024*1024;
+
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
+        if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
+            echo "File berhasil diunggah.<br>";
+            // Display the uploaded image as a thumbnail
+            echo "<img src='$targetfile' width='200' style='height: auto;'><br>";
+        } else {
+            echo "Gagal mengunggah file.";
+        }
+    } else {
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
+    }
+}
+?>
