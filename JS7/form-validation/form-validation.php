8668e11e4c3e61daa7f3057c9a6a1da216bfa382
Muhammad Afif Khosyidzaki <afifkhosyidzaki@gmail.com>
2024-10-15 09:09:42 +0700

    change the code js 7

--- JS7/form-validation/form-validation.php
+++ JS7/form-validation/form-validation.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     <h1>Form Input dengan Validasi</h1>
-    <form method="post" action="proses_validasi.php">
+    <form method="post" action="validation.php">
         <label for="nama">Nama:</label>
         <input type="text" id="nama" name="nama">
         <br>
@@ -18,33 +18,3 @@
     </form>
 </body>
 </html>
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nama = $_POST['nama'];
-    $email = $_POST['email'];
-    $errors = array();
-
-    // Validasi Nama
-    if (empty($nama)) {
-        $errors[] = "Nama harus diisi.";
-    }
-
-    // Validasi Email
-    if (empty($email)) {
-        $errors[] = "Email harus diisi.";
-    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = "Format email tidak valid.";
-    }
-
-    // Jika ada kesalahan validasi
-    if (!empty($errors)) {
-        foreach ($errors as $error) {
-            echo $error . "<br>";
-        }
-    } else {
-        // Lanjutkan dengan pemrosesan data jika semua validasi berhasil
-        // Misalnya, menyimpan data ke database atau mengirim email
-        echo "Data berhasil dikirim: Nama = $nama, Email = $email";
-    }
-}
-?>
