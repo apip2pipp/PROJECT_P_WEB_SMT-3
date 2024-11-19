77fa89c28d53557e7bade9ec1b3b7382ab9e6ab2
Muhammad Afif Khosyidzaki <afifkhosyidzaki@gmail.com>
2024-11-19 10:55:14 +0700

    koneksi JS 12

--- JS10-02/AJAX/koneksi.php
+++ JS10-02/AJAX/koneksi.php
@@ -2,7 +2,7 @@
 define('HOST', 'localhost');
 define('USER', 'root');
 define('PASS', '');
-define('DB1', 'prakwebdb');
+define('DB1', 'prakwebdb2');
 
 // Buat Koneksinya
 $db1 = new mysqli(HOST, USER, PASS, DB1);
